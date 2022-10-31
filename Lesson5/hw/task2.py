09c9e604ffde0302455a9900a3689ac8c44c977a
MIKHAIL CHISLOV <mihailchislov@gmail.com>
2022-10-31 23:06:44 +0300

    Version 01 for review

--- Lesson5/hw/task2.py
+++ Lesson5/hw/task2.py
@@ -1,9 +1,10 @@
 import sqlite3
+
 conn = sqlite3.connect('example.sqlite')
 cursor = conn.cursor()
 data = cursor.execute('SELECT name FROM sqlite_master')
 data = cursor.fetchall()
-print(data)# will be a list with data.
+print(data)  # will be a list with data.
 length = cursor.execute('SELECT count(*) FROM presidents')
 print(length.fetchone()[0])
 
@@ -19,23 +20,14 @@ class RequestTableData():
         conn = sqlite3.connect(database_path)
         return conn.cursor()
 
-    def request_table_data(self, database_path, table_name, data_filter):
-        db_cursor = self.connect_to_database(self, database_path)
-        if data_filter!=None:
-            data = db_cursor.execute('SELECT name FROM ' + table_name + ' where name LIKE \'%' + data_filter + '%\'')
-        else:
-            data = db_cursor.execute('SELECT name FROM ' + table_name)
-
-        return data
-
-
 class TableData():
     def __init__(self, database_name: str, table_name: str, data_filter=None):
         self.database_name = database_name
         self.table_name = table_name
         self.data_filter = data_filter
         self.db_cursor = RequestTableData(database_name).cursor
-        #self.__dict__ = self.get_dict_data(self.path)
+        self.initial_state = self.db_cursor.execute('SELECT * FROM ' + self.table_name)
+        # self.__dict__ = self.db_cursor.fetchone()
 
     def __setitem__(self, key, item):
         self.__dict__[key] = item
@@ -51,11 +43,26 @@ class TableData():
 
     def __len__(self):
         length = self.db_cursor.execute('SELECT count(*) FROM ' + self.table_name)
-        return (length.fetchone()[0])
+        return length.fetchone()[0]
+
+    def __iter__(self):
+        table_rows = len(self)
+        fixed_data = self.db_cursor.execute('SELECT * FROM ' + self.table_name)
+        i = 0
+        while i < table_rows:
+            i += 1
+            iter_data = fixed_data.fetchone()
+            yield iter_data
 
 tableDataItem = TableData('example.sqlite', 'presidents')
 print(len(tableDataItem))
 print(tableDataItem['Yeltsin'])
 
 presidents = TableData(database_name='example.sqlite', table_name='presidents')
-boolean = ('Yeltsin' in presidents)
+if ('Trump', 1337, 'US') in presidents:
+    print('yes')
+else:
+    print('no')
+
+for president in presidents:
+    print(president)
