061a4d2eee38a1c9e19034afcb5dab6e7f01ef91
MIKHAIL CHISLOV <MChislov@users.noreply.github.com>
2022-11-09 17:04:15 +0300

    Update hw_ig.py
    two versions of merge function

--- Lesson6/hw/hw_ig.py
+++ Lesson6/hw/hw_ig.py
@@ -2,6 +2,8 @@
 # non-iterables and elements from iterables (any nesting depth should be supported)
 # function should return an iterator (generator function)
 # don't use third-party libraries
+from typing import Iterable
+
 
 def merge_elems(*elems):
 
@@ -15,11 +17,15 @@ def merge_elems(*elems):
 
     def flatten_element(input_item):
         for element_flatten in input_item:
+            element_index = input_item.index(element_flatten)
             if hasattr(element_flatten, '__iter__') and (len(element_flatten)) > 1:
-                element_index = input_item.index(element_flatten)
                 input_item.pop(element_index)
                 for item_index in range(len(element_flatten)):
                     input_item.insert(item_index + element_index, element_flatten[item_index])
+            else:
+                if hasattr(element_flatten, '__iter__'):
+                    input_item.pop(element_index)
+                    input_item.insert(element_index, element_flatten[0])
         return input_item
 
     for item in elems:
@@ -31,14 +37,43 @@ def merge_elems(*elems):
         else:
             yield item
 
+def merge(*elems):
+    def submerge(item):
+        for sub_item in item:
+            if isinstance(sub_item, Iterable) and not isinstance(sub_item, (str, bytes)):
+                for sub_sub_item in submerge(sub_item):
+                    yield sub_sub_item
+            else:
+                if isinstance(sub_item, str):
+                    for char in sub_item:
+                        yield char
+                else:
+                    yield sub_item
+
+    for elem in elems:
+        if isinstance(elem, Iterable) and not isinstance(elem, (str, bytes)):
+            for sub_elem in submerge(elem):
+                yield sub_elem
+        else:
+                if isinstance(elem, str):
+                    for char in elem:
+                        yield char
+                else:
+                    yield elem
+
 
 # example input
 a = [1, 2, 3]
 b = 6
 c = 'zhaba'
-d = [[1, 2], [3, 4]]
+d = [[1, 2], [3, [4,6]], [5]]
 
+print("Fixed first:\n")
 for _ in merge_elems(a, b, c, d):
+    print(_, end=' ')
+
+print("\nNew method:\n")
+for _ in merge(a, b, c, d):
     print(_, end=' ')
 
 # output: 1 2 3 6 z h a b a 1 2 3 4
