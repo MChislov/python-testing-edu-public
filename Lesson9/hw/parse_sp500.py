6405072e42cfdba5c4587e450f522222a861c83c
MIKHAIL CHISLOV <mihailchislov@gmail.com>
2022-11-11 20:52:30 +0300

    02 USD value retrieving added

--- Lesson9/hw/parse_sp500.py
+++ Lesson9/hw/parse_sp500.py
@@ -1,4 +1,5 @@
 import requests
+import xmltodict
 import re
 from bs4 import BeautifulSoup
 from lxml import etree
@@ -8,20 +9,20 @@ root_url = "https://markets.businessinsider.com"
 cbrf_cur_url = "https://www.cbr.ru/scripts/XML_daily.asp?date_req="
 companies = []
 
+
 def get_page_html(url):
     html_text = requests.get(url).text
     return html_text
 
+
 def get_usr_rub():
     curdate = datetime.today().strftime('%d/%m/%Y')
-    data = get_page_html(cbrf_cur_url + curdate)
-    print(data)
-    tree = etree.ElementTree.parse(data)
-    for child in tree.getroot():
-        print(child.tag)
+    response = requests.get(cbrf_cur_url + curdate)
+    datadict = xmltodict.parse(response.content)
+    for item in datadict['ValCurs']['Valute']:
+        if item['CharCode'] == 'USD':
+            return item['Value']
 
-
-    print(curdate)
 
 def get_companies_details_main_page(index):
     text = get_page_html(root_url + '/index/components/s&p_500?p=' + str(index))
@@ -29,10 +30,9 @@ def get_companies_details_main_page(index):
     r = tree.xpath('//div[@class= "table-responsive"]//tbody/tr/td/a')
     year = tree.xpath('//div[@class= "table-responsive"]//tbody/tr/td[8]/span[2]')
     for line, values in zip(r, year):
-        companies.append({line.text: [ {"url": root_url + line.get("href")}, {"year": values.text}]})
-
-#get_companies_details_main_page(1)
-#print(companies)
-get_usr_rub()
+        companies.append({line.text: [{"url": root_url + line.get("href")}, {"year": values.text}]})
 
 
+# get_companies_details_main_page(1)
+# print(companies)
+print(get_usr_rub())
