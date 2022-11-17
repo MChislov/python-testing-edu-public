91c83e434fcfaa49a488573639d4081764f30c25
MIKHAIL CHISLOV <mihailchislov@gmail.com>
2022-11-17 11:44:23 +0300

    v.1.1 for review

--- Lesson13/hw/selenium_wd_hw.py
+++ Lesson13/hw/selenium_wd_hw.py
@@ -4,13 +4,9 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.get("https://www.saucedemo.com")
 assert "Swag Labs" in driver.title
-username_element = driver.find_element(By.NAME, "user-name")
-username_element.clear()
-username_element.send_keys("standard_user")
-password_element = driver.find_element(By.ID, "password")
-password_element.clear()
-password_element.send_keys("secret_sauce")
-login_button_elem = driver.find_element(By.XPATH, "//*[@type='submit']")
-login_button_elem.click()
+add_username = driver.find_element(By.NAME, "user-name").send_keys("standard_user")
+add_password = driver.find_element(By.ID, "password").send_keys("secret_sauce")
+click_login_button = driver.find_element(By.XPATH, "//*[@type='submit']").click()
 assert "Epic sadface: Username and password do not match any user in this service" not in driver.page_source
+assert "Sauce Labs Backpack" in driver.page_source
 driver.close()
